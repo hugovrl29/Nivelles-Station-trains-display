f6f46ea9d426c560dee9bdfa9b0de396e067966e
Hugo Verly <verlyhugo959@gmail.com>
2025-10-25 13:01:33 +0200

    API info get

--- nivelles-train-dashboard/src/App.jsx
+++ nivelles-train-dashboard/src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { fetchIRail } from './utils/fetchStation';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
+  const [departures, setDepartures] = useState([])
+  const [loading, setLoading] = useState(true)
+
+
+  useEffect(() => {
+
+    async function loadData() {
+      setLoading(true);
+      const data = await fetchIRail("liveboard", {arrdep: "departure"});
+
+      if (data) {
+        setDepartures(data.departures?.departure.slice(0, 8) || []);
+      }
+      setLoading(false);
+    }
+  loadData();
+
+  }, [])
+
+  if (loading) {
+    return <p> Loading departures...</p>;
+  }
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="p-4">
+      <h1>Next 3 Departures from Nivelles</h1>
+      <ul>
+        {departures.map((train, index) => {
+
+          const type = train.vehicleinfo?.type || "?";
+
+          const time = new Date(train.time * 1000).toLocaleTimeString([], {hour: "2-digit", minute: "2-digit" });
+
+          const delayMin = Math.floor((parseInt(train.delay || 0, 10)) / 60);
+
+          let status = "On time";
+          if (train.canceled === "1") {
+            status = "Canceled"
+          } else if (delayMin > 0){
+            status = `Delayed +${delayMin} min`;
+          }
+
+          return (
+            <li key={index}>
+              {type} - {time} - {train.station} {type !== "BUS" ? `- Track ${train.platforminfo.normal}` : ""} - {status}
+            </li>
+          )
+        })}
+      </ul>
+    </div>
   )
 }
 
